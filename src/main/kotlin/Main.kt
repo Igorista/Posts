7017647e1d8f39380be2eae817c8d078abd088a5
Igorista <minelab08@gmail.com>
2021-12-20 20:14:12 +0300

    Exeption last

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,31 +3,31 @@ fun main(args: Array<String>) {
     Attachments.attachAdd(AudioAttachment("audio", Audio(1, 2, 3, 4)))
     Attachments.attachAdd(LinkAttachment("link", Link(2, 2, 2, 2)))
     var attach1 = Attachments.attachments
-    Wallservice.add(Post(0, 12, 22, 32, 81221, attach1))
-    Wallservice.add(Post(0, 11, 65, 232, 91221, null))
-    Attachments.attachAdd(LinkAttachment("link", Link(3, 3, 3, 3)))
+    var posts = Wallservice()
+    posts.addPost(Post(0, 12, 22, 32, 81221, attach1))
+    posts.addPost(Post(0, 11, 65, 232, 91221, null))
+    Attachments.attachAdd(LinkAttachment("link", Link(1, 3, 3, 3)))
     attach1 = Attachments.attachments
-    Wallservice.update(Post(1, 16, 123, 3435, 101221, attach1))
-    Wallservice.createComment(Comment(55, 12, 161221, "Привет!"))
-    println(Wallservice.posts.joinToString())
-    println(Wallservice.comments.joinToString())
-
+    posts.updatePost(Post(1, 16, 123, 3435, 101221, attach1))
+    posts.createComment(Comment(3, 12, 161221, "Привет!"))
+    println(posts.posts.joinToString())
+    println(posts.comments.joinToString())
 }
 
-object Wallservice {
+class Wallservice {
     var posts: Array<Post> = emptyArray()
         private set
-    var comments = emptyArray<Comment>()
+    var comments: Array<Comment> = emptyArray()
         private set
 
-    fun add(post: Post): Post {
+    fun addPost(post: Post): Post {
         posts += post
         val notEmpty = posts.last()
         notEmpty.id = posts.size.toLong()
         return posts.last()
     }
 
-    fun update(post: Post): Boolean {
+    fun updatePost(post: Post): Boolean {
         var postUpdated = false
         for (i in posts.indices) {
             when {
@@ -47,12 +47,14 @@ object Wallservice {
     }
 
     fun createComment(comment: Comment) {
-        for (i in posts.indices){
-            when {
-                posts[i].id == comment.postId -> comments += comment
-                }
+        var flag = false
+        for (i in posts.indices) {
+            if (posts[i].id == comment.postId) {
+                comments += comment
+                flag = true
             }
-        if(comments.isEmpty()) throw PostNotFoundException("Пост с id ${comment.postId} не существует!")
+        }
+        if (flag == false) throw PostNotFoundException("Пост с id ${comment.postId} не существует!")
     }
 }
 
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -2,15 +2,16 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
+
 class WallServiceTest {
     @Test
     fun postExisting() {
         val result: Boolean
-        val service = Wallservice
+        var service = Wallservice()
         val attach = Attachments
         attach.attachAdd(VideoAttachment("video", Video(1,1,1,1)))
         val videoAdd = attach.attachments
-        val add = service.add(Post(0,111,123,12312,123123,videoAdd))
+        val add = service.addPost(Post(0,111,123,12312,123123,videoAdd))
         val(id,_ownerId,_fromId,_createdBy,_date,_attachment) = add
         when {
             id > 0 -> result = true
@@ -20,30 +21,30 @@ class WallServiceTest {
     }
     @Test
     fun updateExistingTrue(){
-        val service = Wallservice
-        service.add(Post(0,111,123,12312,123123,null))
-        service.add(Post(0,1,112,558,88888,null))
-        service.add(Post(0,5551,515,879,797979,null))
+        val service = Wallservice()
+        service.addPost(Post(0,111,123,12312,123123,null))
+        service.addPost(Post(0,1,112,558,88888,null))
+        service.addPost(Post(0,5551,515,879,797979,null))
         val update = Post(1,5551,611544,445,797979,null)
-        val result = service.update(update)
+        val result = service.updatePost(update)
         assertTrue(result)
     }
     @Test
     fun updateExistingFalse() {
-        val service = Wallservice
-        service.add(Post(0, 111, 123, 12312, 123123,null))
-        service.add(Post(0, 1, 112, 558, 88888,null))
-        service.add(Post(0, 5551, 515, 879, 797979,null))
+        val service = Wallservice()
+        service.addPost(Post(0, 111, 123, 12312, 123123,null))
+        service.addPost(Post(0, 1, 112, 558, 88888,null))
+        service.addPost(Post(0, 5551, 515, 879, 797979,null))
         val update = Post(5, 1616, 611544, 445, 44444,null)
-        val result = service.update(update)
+        val result = service.updatePost(update)
         assertFalse(result)
     }
     @Test
     fun createCommentTrue(){
-        val service = Wallservice
+        val service = Wallservice()
         val result: Boolean
-        service.add(Post(0, 111, 123, 12312, 123123,null))
-        service.add(Post(0, 1, 112, 558, 88888,null))
+        service.addPost(Post(0, 111, 123, 12312, 123123,null))
+        service.addPost(Post(0, 1, 112, 558, 88888,null))
         service.createComment(Comment(1, 12, 161221, "Привет!"))
         when {
             service.comments.isEmpty() -> result = false
@@ -53,9 +54,9 @@ class WallServiceTest {
     }
     @Test(expected = PostNotFoundException::class)
     fun shouldThrow() {
-        val service = Wallservice
-        service.add(Post(0, 111, 123, 12312, 123123,null))
-        service.add(Post(0, 1, 112, 558, 88888,null))
+        val service = Wallservice()
+        service.addPost(Post(0, 111, 123, 12312, 123123,null))
+        service.addPost(Post(0, 1, 112, 558, 88888,null))
         service.createComment(Comment(10, 12, 161221, "Привет!"))
         service.comments.isEmpty()
     }
